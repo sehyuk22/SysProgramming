8f6740a2cc38a1f678a05f1e477bdd19bc7f3375
sehyuk22 <71517406+sehyuk22@users.noreply.github.com>
2021-04-05 22:24:57 +0900

    Add files via upload

---
+++ 0405/makeTest/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "copy.h"
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int main()
+{
+int len;
+int max;
+max = 0;
+while (fgets(line, 100, stdin) != NULL) {
+len = strlen(line);
+if (len > max) {
+max = len;
+copy(line, longest);
+}
+}
+if (max > 0)
+printf("%s", longest);
+return 0;
+}
